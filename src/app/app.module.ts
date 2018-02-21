b9ccd0fc488af348228786040904e72a5e5d8cbb
Lauri Svan <lauri.svan@specsign.com>
2018-02-21 22:29:43 +0200

    Rudimentary ngsw service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
-
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 
@@ -10,7 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    environment.production ?
+      ServiceWorkerModule.register('/ngsw-worker.js') :
+      []
   ],
   providers: [],
   bootstrap: [AppComponent]
