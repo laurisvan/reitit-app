e7730108e6c7c9852f643a239b43acddaedd31d3
Lauri Svan <lauri.svan@specsign.com>
2018-02-22 08:56:47 +0200

    Rudimentary Workbox service worker

---
+++ build/generate-service-worker.js
@@ -0,0 +1,12 @@
+// Inside of build.js:
+const workboxBuild  = require('workbox-build');
+
+const swDest = 'dist/sw.js';
+workboxBuild.injectManifest({
+  swSrc: 'src/sw.js',
+  swDest,
+  globDirectory: 'dist',
+  // Other configuration options...
+}).then(({count, size}) => {
+  console.log(`Generated ${swDest}, which will precache ${count} files, totaling ${size} bytes.`);
+});
